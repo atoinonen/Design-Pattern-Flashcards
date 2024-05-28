68d38bf04267596d3411ab0de1f56f9b3eb67d6f
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-05-28 17:53:07 +0300

    Add background colors to cards
    Adds background colors to design pattern cards based on the type of the
    pattern. Also increase the size of the font of the name of the design
    pattern.

--- app/src/main/java/com/example/flashcards/designpatterns/DesignPatternScreen.kt
+++ app/src/main/java/com/example/flashcards/designpatterns/DesignPatternScreen.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.flashcards.designpatterns.model.DesignPattern
 import com.example.flashcards.designpatterns.model.DesignPatternsRepository
 import com.example.flashcards.designpatterns.model.PatternType
@@ -35,10 +39,20 @@ fun DesignPatternListItem(
     designPattern: DesignPattern,
     modifier: Modifier = Modifier
 ) {
-    Card(modifier = modifier.fillMaxWidth()) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = when (designPattern.type) {
+                PatternType.CREATIONAL -> Color(0xFFFAD5D5)
+                PatternType.STRUCTURAL -> Color(0xFFD4FFB1)
+                PatternType.BEHAVIORAL -> Color(0xFFC0D5FF)
+            }
+        )
+    ) {
         Column() {
             Text(
                 text = stringResource(id = designPattern.name),
+                fontSize = 30.sp,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
             Text(text = stringResource(id = designPattern.description))
