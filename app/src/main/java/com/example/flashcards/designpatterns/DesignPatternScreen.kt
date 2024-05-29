05f1891a88dbd9958a4a6cbbd75a31b04e590e1b
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-05-29 22:59:23 +0300

    Organize card text better for longer strings
    Organizes card's description text better for longer paragraphs of text.
    Also fixes clipping issues with url from card's rounded corner that
    happens with full wall of text.

--- app/src/main/java/com/example/flashcards/designpatterns/DesignPatternScreen.kt
+++ app/src/main/java/com/example/flashcards/designpatterns/DesignPatternScreen.kt
@@ -19,6 +19,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.flashcards.designpatterns.model.DesignPattern
@@ -76,11 +79,19 @@ fun DesignPatternListItem(
             )
             Text(
                 text = stringResource(id = designPattern.description),
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    hyphens = Hyphens.Auto,
+                    lineBreak = LineBreak.Paragraph.copy(strategy = LineBreak.Strategy.HighQuality)
+                ),
+                overflow = TextOverflow.Ellipsis,
+                modifier = modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f)
             )
             Text(
                 text = stringResource(id = designPattern.url),
-                style = MaterialTheme.typography.bodySmall
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(horizontal = 4.dp)
             )
         }
     }
