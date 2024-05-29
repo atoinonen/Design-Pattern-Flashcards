3d144b795311c051858b069d74961950eb4e0aeb
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-05-29 13:10:41 +0300

    Change the orientation to a row of cards
    Changes the orientation of the cards from a column to a row and each
    card is the size of the screen.

--- app/src/main/java/com/example/flashcards/designpatterns/DesignPatternScreen.kt
+++ app/src/main/java/com/example/flashcards/designpatterns/DesignPatternScreen.kt
@@ -1,10 +1,17 @@
 package com.example.flashcards.designpatterns
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
@@ -13,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,12 +30,18 @@ import com.example.flashcards.designpatterns.model.DesignPatternsRepository
 import com.example.flashcards.designpatterns.model.PatternType
 import com.example.flashcards.designpatterns.ui.theme.DesignPatternFlashcardsTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DesignPatternList(
     designPatterns: List<DesignPattern>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+    val state =  rememberLazyListState()
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        state = state,
+        flingBehavior = rememberSnapFlingBehavior(lazyListState = state)
+    ) {
         items(designPatterns) { designPattern ->
             DesignPatternListItem(designPattern = designPattern)
         }
@@ -40,7 +54,9 @@ fun DesignPatternListItem(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxHeight()
+            .width(LocalConfiguration.current.screenWidthDp.dp),
         colors = CardDefaults.cardColors(
             containerColor = when (designPattern.type) {
                 PatternType.CREATIONAL -> Color(0xFFFAD5D5)
